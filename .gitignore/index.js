07c0f9097d2c437c271565bc75c2f3ae5e4768b3
UnderstandF <35169622+UnderstandF@users.noreply.github.com>
2018-01-06 14:39:47 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ('*')
+
+bot.on('ready', function() {
+    bot.user.setGame("Summoners Wars");
+    console.log("Connectedç");
+});
+
+bot.login("MzY5MTcwNDU4NzMxNjEwMTEz.DTGRVw.PU1aH8ncvLMTRdjc2WaaiU1aVoY");
+
+
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes: \n -*help");
+    }
+
+    if (message.content === "Tg"){
+        message.reply("Malpoli !");
+        console.log("Quelqu'un à dis tg");
+    }
+      
+
+    if (message.content === "Salut"){
+        message.reply("Bien le bonjour.");
+        console.log("Commande Salut effectué"); 
+    }
+});
